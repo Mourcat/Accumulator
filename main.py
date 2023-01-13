e442c29ed53be4db395f2b316be4cc69b300c114
Mourcat <vybornovs@rambler.ru>
2023-01-13 21:14:41 +0300

    Dancing with Kivy and Java, Pyjnius and Android.

--- main.py
+++ main.py
@@ -33,17 +33,21 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.card import MDCard
 from kivymd.uix.button import MDRaisedButton, MDRectangleFlatButton, MDFloatingActionButton
 from kivymd.uix.dialog import MDDialog
+from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.templates import RotateWidget
-print(dir(Image))
+from kivymd.toast import toast
 
 
+class SettingsCls(MDBoxLayout):
+    '''Class emplements settings dialog  layout'''
+    
+    
 class ImgCard(MDCard, ButtonBehavior):
     '''Class emplements image card with button behaviour'''
     source = StringProperty()
     text = StringProperty()
-    img_width = NumericProperty(0)
-    img_height = NumericProperty(0)
+    img_size = StringProperty()
 
 
 class MagicIcoButton(MagicBehavior, MDFloatingActionButton):
@@ -71,19 +75,21 @@ class Cam(CircularElevationBehavior, Camera, RotateWidget):
         
     def burst(self, dt):
         self.t += round(dt)
-        print(self.t)
         if self.t <= self.quantity:
             app = MDApp.get_running_app()
             self.take_pic()
             app.root.ids.burst_btn.wobble()
         else: 
             Clock.unschedule(self.burst)
+            toast(f'{self.quantity} images saved')
         
     def take_pic(self):
         pics = [f for f in os.listdir(f'{PATHS["data"]}') if f.startswith("img")]
         index= len(pics)+1
         img = CoreImage(self.get_texture)
-        img.save(f'{PATHS["data"]}/img{index}.jpg')
+        filename_to_save = f'{PATHS["data"]}/img{index}.jpg'
+        img.save(filename_to_save)
+        toast(f'Image img{index}.jpg saved')
     
 
 class InScreen(MDScreen):
@@ -91,20 +97,17 @@ class InScreen(MDScreen):
     dialog =None
     cam = ObjectProperty()
     img_list = None
-    
-    def __init__(self, **kwargs):
-        super(InScreen, self).__init__(**kwargs)
         
     def read_storage_data(self):
         self.img_list = [f for f in os.listdir(PATHS['data']) if f.endswith('.jpg') or f.endswith('.jpeg') or f.endswith('.png')]
-        self.ids.bakground.source = f'data/{random.choice(self.img_list)}'
+        self.ids.background.source = f'data/{random.choice(self.img_list)}'
         Animation(opacity=1).start(self.ids.work_lay)
      
     def settings_dialog(self):
         self.dialog = MDDialog(
             title='Settings',
             type='custom',
-            #content_cls
+            content_cls=SettingsCls(),
             buttons=[
                 MDRectangleFlatButton(
                     text='CANCEL',
@@ -124,6 +127,7 @@ class DataAccumulatorApp(MDApp):
         self.theme_cls.material_style = 'M3'
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Lime'
+        self.theme_cls.accent_palette = 'Orange'
         self.root = InScreen()
         return self.root
         
@@ -137,10 +141,10 @@ class DataAccumulatorApp(MDApp):
                 source=f'{PATHS["data"]}/{img}',
                 text = f'{img}',
             ))
+        for child in self.root.ids.grid.children:
+            w, h = Image(source=f'{child.source}').texture_size
+            child.ids.item_lbl.text = f'{child.text} • {w}×{h} px'
             
-            self.root.ids.grid.children[-1].img_width = Image(source=f'{PATHS["data"]}/{img}').texture_size[0]
-            self.root.ids.grid.children[-1].img_height = Image(source=f'{PATHS["data"]}/{img}').texture_size[1]
-        
 
 if __name__ == '__main__':
     DataAccumulatorApp().run()
